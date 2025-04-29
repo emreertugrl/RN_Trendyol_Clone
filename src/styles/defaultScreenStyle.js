d17af14a95bd01c7d4401fc7abdc551c60fbc902
emreertugrl <emrertugrl@gmail.com>
2025-04-29 17:20:18 +0300

    default container styles

---
+++ src/styles/defaultScreenStyle.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+import Colors from '../theme/colors';
+
+export const defaultScreenStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.WHITE,
+    padding: 10,
+  },
+});
